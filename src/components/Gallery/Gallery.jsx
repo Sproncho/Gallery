5db1106ce3208982aedf37c7a8a272ded2e133f2
Sproncho <dan.zaborov@gmail.com>
2023-02-25 23:35:27 +0300

    initial commit

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -9,43 +9,47 @@ import img6 from '../../images/friend6.jpg'
 import img7 from '../../images/friend7.jpg'
 import img8 from '../../images/friend8.jpg'
 import img9 from '../../images/friend9.jpg'
-let images = [img1,img2,img3,img4,img5,img6,img7,img8,img9];
+
+let imagesSrc = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
 function Gallery(props) {
-    class Image{
-        constructor(src,state) {
+    class Image {
+        constructor(src, state) {
             this.src = src;
             this.state = state;
         }
     }
-    useEffect(()=>{
-        images = images.map(img => new Image(img,'visible'));
-        setSelected(null);
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(imagesSrc.map(src => new Image(src, 'visible')))
     },[images])
-    const [selected,setSelected] = useState(null);
-    function setImage(image){
-       if(selected === null){
-           image.state = 'selected';
-           images.forEach(image => {
-               if(image.state !== 'selected')
-                   image.state = 'hidden';
-           })
-           setSelected(image)
-       }else if(selected === image){
-           images.forEach(image => image.state = 'visible')
-           setSelected(null);
-       }
+
+    function setImage(image) {
+        const imgs = [...images];
+        const img = imgs.find(img => img.src === image.src);
+        if (image.state === 'visible') {
+            img.state = 'selected';
+            img.forEach(image => {
+                if (image.state !== 'selected')
+                    image.state = 'hidden';
+            })
+        } else if (image.state === 'selecyed') {
+            img.forEach(image => image.state = 'visible')
+        }
+        setImages(imgs);
     }
 
     let handleClick = (e) => {
 
-        let image = images[parseInt(e.target.id)]
-        console.log(image)
+        let image = images[parseInt(e.target.id)];
+        console.log(image);
         setImage(image);
     }
     return (
-        <div className='gallery' >
-            {images.map((img,i) => <img onClick={handleClick} className={img.state|'visible'} key={i} id={i.toString()} src={img}/>)}
+        <div className='gallery'>
+            {images.map((img, i) => <img onClick={handleClick} className={img.state} key={i} id={i.toString()} src={img.src}/>)}
         </div>
     );
 }
